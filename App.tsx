f17a33ccb204235b9f214b88f93cee1cf0c313a3
Wesley Scholl <clients@konjoweb.com>
2023-01-31 22:27:31 -0700

    skeleton, list, hit api getting response, setting up to access api data and render in list

--- App.tsx
+++ App.tsx
@@ -1,18 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native';
+
+import { useEffect } from 'react';
+import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+import { FlashList } from "@shopify/flash-list";
+
+const DATA = [{title: "First Item"},{title: "Second Item"},{title: "First Item"},{title: "First Item"},{title: "First Item"},{title: "First Item"},{title: "First Item"},{title: "First Item"},];
 
 export default function App() {
+
+  useEffect(() => {
+    getCoins();
+  }, [])
+  
+  function getCoins() {
+        fetch("https://api.coinlore.net/api/tickers/", {
+          method: "GET"
+        }).then(res => res.json())
+          .then(res => {
+           console.log(res)
+          }).catch(error => {
+            console.log(`${error.message}!`);
+          });
+    }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <ScrollView  contentContainerStyle={{marginTop: 50}}>
+      <Text style={{fontSize: 32, textAlign: 'center', fontWeight: '300'}}>Cryptocurrencies</Text>
+      <FlashList
+      ListHeaderComponent={<View style={{alignItems:'center', justifyContent: 'space-around', flexDirection:'row', marginTop: 20}}>
+        <Text style={{fontSize: 10, textAlign: 'center', fontWeight: '300'}}>Name</Text>
+        <Text style={{fontSize: 10, textAlign: 'center', fontWeight: '300'}}>Symbol</Text>
+        <Text style={{fontSize: 10, textAlign: 'center', fontWeight: '300'}}>Price ($USD)</Text>
+        </View>}
+      data={DATA}
+      renderItem={({ item }) => <View style={{alignItems:'center', justifyContent: 'space-around', flexDirection:'row', padding:10}}>
+      <Text style={{fontSize: 18}}>{item.title}</Text>
+      <Text style={{fontSize: 18}}>{item.title}</Text>
+      <Text style={{fontSize: 18}}>{item.title}</Text></View>}
+      estimatedItemSize={200}
+    />
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
